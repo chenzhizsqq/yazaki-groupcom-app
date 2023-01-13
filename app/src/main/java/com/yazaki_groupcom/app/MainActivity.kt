55d413c66a46349442a9af8a23b8baaefd8ea9b6
chenzhizsqq <chenzhi@qq.com>
2023-01-13 17:57:48 +0900

    T1001 QRコードログイン - 100% 添加二维码的扫描功能

--- app/src/main/java/com/yazaki_groupcom/app/MainActivity.kt
+++ app/src/main/java/com/yazaki_groupcom/app/MainActivity.kt
@@ -1,18 +1,66 @@
 package com.yazaki_groupcom.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 import com.yazaki_groupcom.app.base.BaseActivity
 import com.yazaki_groupcom.app.databinding.ActivityMainBinding
+
 
 class MainActivity : BaseActivity() {
 
     val TAG: String = "MainActivity"
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var qrScanIntegrator: IntentIntegrator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.etQrCode.setOnClickListener {
+            qrScan()
+        }
+
+        binding.tvQrMessage.setOnClickListener {
+            qrScan()
+        }
+
+        binding.btIdLogin.setOnClickListener {
+
+        }
+
+        binding.btRfid.setOnClickListener {
+
+        }
+
+
+
     }
 
+    //QR SCAN
+    private fun qrScan() {
+        this.qrScanIntegrator = IntentIntegrator(this)
+        // 縦画面に固定
+        this.qrScanIntegrator.setOrientationLocked(false)
+        // QRコード読み取り後のビープ音を停止
+        this.qrScanIntegrator.setBeepEnabled(false)
+        // スキャン開始
+        this.qrScanIntegrator.initiateScan()
+    }
+
+    // 読取後に呼ばれる
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null) {
+            // 読取結果はresult.contentsで参照できる
+            Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+            binding.etQrCode.setText(result.contents)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 }
