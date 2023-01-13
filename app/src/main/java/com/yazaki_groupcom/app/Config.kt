39a2efa151fc27e4691a49de5eedea65159aca2b
chenzhizsqq <chenzhi@qq.com>
2023-01-13 10:11:28 +0900

    1.添加BaseActivity 2.添加Config

---
+++ app/src/main/java/com/yazaki_groupcom/app/Config.kt
@@ -0,0 +1,19 @@
+package com.yazaki_groupcom.app
+
+class Config {
+    companion object {
+        const val ApiUrl = "https://localhost/"
+
+        // 連打判断間隔msec
+        const val DOUBLE_CLICK_INTERVAL = 500L
+
+        const val Device = "android"
+
+        //初回登録Frag key
+        const val FragKey = "selectFragKey"
+
+        //LOCATION KEY
+        const val REQUEST_CODE_LOCATION_KEY = 10008
+    }
+
+}
---
+++ app/src/main/java/com/yazaki_groupcom/app/base/BaseActivity.kt
@@ -0,0 +1,24 @@
+package com.yazaki_groupcom.app.base
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * ベースアクティビティ
+ * アクティビティ共通処理をここで処理する。
+ */
+open class BaseActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 縦画面
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
+        // タイトルバー非表示
+        supportActionBar?.hide()
+    }
+
+}
