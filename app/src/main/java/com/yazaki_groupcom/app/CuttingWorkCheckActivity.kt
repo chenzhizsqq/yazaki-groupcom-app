f7d300f2ebb5ba82a37abd28f6d6841d56cacde8
chenzhizsqq <chenzhi@qq.com>
2023-01-17 10:03:37 +0900

    T3004＿切断作業チェック/単線 - 10% 新建一个Activity

---
+++ app/src/main/java/com/yazaki_groupcom/app/CuttingWorkCheckActivity.kt
@@ -0,0 +1,14 @@
+package com.yazaki_groupcom.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 切断作業チェック
+ */
+class CuttingWorkCheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cutting_work_check)
+    }
+}
