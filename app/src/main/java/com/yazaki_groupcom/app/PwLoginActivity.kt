62df6d1b2b73b85e8bc1a94c1f5b67c886c06241
chenzhizsqq <chenzhi@qq.com>
2023-01-18 15:31:08 +0900

    T1003＿ ID入力ログイン(2/2）- 100%

--- app/src/main/java/com/yazaki_groupcom/app/PwLoginActivity.kt
+++ app/src/main/java/com/yazaki_groupcom/app/PwLoginActivity.kt
@@ -2,10 +2,14 @@ package com.yazaki_groupcom.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
 import com.yazaki_groupcom.app.base.BaseActivity
 import com.yazaki_groupcom.app.databinding.ActivityPwLoginBinding
+import androidx.appcompat.widget.PopupMenu
 
-class PwLoginActivity : BaseActivity() {
+class PwLoginActivity : BaseActivity(),PopupMenu.OnMenuItemClickListener {
     companion object {
         const val TAG: String = "PwLoginActivity"
     }
@@ -22,6 +26,41 @@ class PwLoginActivity : BaseActivity() {
 //            overridePendingTransition( android.R.anim.slide_in_left,
 //                android.R.anim.slide_out_right);
             finish()
+        }
+
+        binding.etId.setOnClickListener {
+            openMenu(it,R.menu.menu_items)
+        }
+    }
+
+    /**
+     * 入力内容に合致する作業者IDを表示
+     */
+    private fun openMenu(anchor: View, resId: Int) {
+        val popup = PopupMenu(this, anchor)
+        popup.inflate(resId)
+        popup.setOnMenuItemClickListener(this)             // リスナーの登録
+        popup.show()
+    }
+
+    /**
+     * メニュークリック
+     */
+    override fun onMenuItemClick(item: MenuItem): Boolean { // リスナーの実装
+        return when (item.itemId) {
+            R.id.menu_item1 -> {
+                Log.e(TAG, "!!! onMenuItemClick: 1" )
+                true
+            }
+            R.id.menu_item2 -> {
+                Log.e(TAG, "!!! onMenuItemClick: 2" )
+                true
+            }
+            R.id.menu_item3 -> {
+                Log.e(TAG, "!!! onMenuItemClick: 3" )
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
