0f56ffd6c43ccedcc2a18523ddcac1b9696eaa06
chenzhizsqq <chenzhi@qq.com>
2023-01-20 10:03:40 +0900

    T3002＿担当AC 選択登録（1/2）- 50%

---
+++ app/src/main/java/com/yazaki_groupcom/app/AcSelectActivity.kt
@@ -0,0 +1,18 @@
+package com.yazaki_groupcom.app
+
+import android.os.Bundle
+import com.yazaki_groupcom.app.base.BaseActivity
+import com.yazaki_groupcom.app.databinding.ActivityAcSelectBinding
+
+class AcSelectActivity : BaseActivity() {
+
+    companion object {
+        const val TAG: String = "AcSelectActivity"
+    }
+    private lateinit var binding: ActivityAcSelectBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAcSelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
