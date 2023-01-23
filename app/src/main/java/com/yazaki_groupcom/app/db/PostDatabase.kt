c846fd62fc9c85d69bd610198d4f9af897ba22a4
chenzhizsqq <chenzhi@qq.com>
2023-01-23 16:04:07 +0900

    本地数据库 - 90%

--- app/src/main/java/com/yazaki_groupcom/app/db/PostDatabase.kt
+++ app/src/main/java/com/yazaki_groupcom/app/db/PostDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 
 //数据库的接口
-@Database(entities = [Post::class, User::class], version = 4)
+@Database(entities = [Post::class, User::class], version = 1)
 abstract class PostDatabase : RoomDatabase() {
     abstract fun postDao(): PostDao
 
--- app/src/main/java/com/yazaki_groupcom/app/db/UserDao.kt
+++ app/src/main/java/com/yazaki_groupcom/app/db/UserDao.kt
@@ -4,6 +4,18 @@ import androidx.room.*
 
 @Dao
 interface UserDao {
-    @Query("SELECT * FROM users")
-    fun getAll(): List<User>
+    @Query("SELECT * FROM m_user")
+    suspend fun getAll(): List<User>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg post: User)
+
+    @Update
+    suspend fun update(vararg users: User)
+
+    @Delete
+    suspend fun delete(post: User)
+
+    @Query("delete FROM m_user")
+    suspend fun deleteAll()
 }
