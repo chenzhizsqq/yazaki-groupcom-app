275e1511ca8af70f54371a05a183bc46e9b78bed
chenzhizsqq <chenzhi@qq.com>
2023-01-27 12:05:12 +0900

    TestScanServiceActivity.kt 用作测试当前手机的QR code专用功能

--- app/src/main/java/com/yazaki_groupcom/app/TestScanServiceActivity.kt
+++ app/src/main/java/com/yazaki_groupcom/app/TestScanServiceActivity.kt
@@ -1,5 +1,6 @@
 package com.yazaki_groupcom.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -18,8 +19,35 @@ class TestScanServiceActivity : AppCompatActivity() {
         binding = ActivityTestScanServiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btTest.setOnClickListener {
-            Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+        //Unitech Scan Serviceの開始
+        binding.tvStart.setOnClickListener {
+            Intent().also { intent ->
+                intent.action = "unitech.scanservice.start"
+                sendBroadcast(intent)
+                Toast.makeText(this, "start", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Scan To Keyを終了する
+        binding.tvScan2keyFalse.setOnClickListener {
+            Intent().also { intent ->
+                intent.action = "unitech.scanservice.scan2key_setting"
+                intent.putExtras(Bundle().apply {
+                    putBoolean("scan2key", false)
+                })
+                sendBroadcast(intent)
+                Toast.makeText(this, "scan2key_setting : scan2key, false", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Unitech Scan Serviceの終了
+        binding.tvClose.setOnClickListener {
+            Intent().also { intent ->
+                intent.action = "unitech.scanservice.close"
+                intent.putExtra("close", true)
+                sendBroadcast(intent)
+                Toast.makeText(this, "close : close, true", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
