f74002bf599c177d5d39fa1e10d51ab9b84bb66e
chenzhizsqq <chenzhi@qq.com>
2023-02-02 10:38:09 +0900

    TestDatePickerActivity.kt 测试：年月日选择 完成了

--- app/src/main/java/com/yazaki_groupcom/app/test/TestDatePickerActivity.kt
+++ app/src/main/java/com/yazaki_groupcom/app/test/TestDatePickerActivity.kt
@@ -1,8 +1,10 @@
 package com.yazaki_groupcom.app.test
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import com.yazaki_groupcom.app.base.BaseActivity
 import com.yazaki_groupcom.app.databinding.ActivityTestDatePickerBinding
+import java.util.*
 
 
 class TestDatePickerActivity : BaseActivity() {
@@ -15,5 +17,23 @@ class TestDatePickerActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTestDatePickerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //"点击选择日期"点击后，显示日期
+        binding.TestDatePickerActivity.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    // do something with the selected date
+                    binding.selectedYear.text = selectedYear.toString()
+                    binding.selectedMonth.text = selectedMonth.toString()
+                    binding.selectedDay.text = selectedDay.toString()
+                }, year, month, day)
+
+            datePickerDialog.show()
+        }
     }
 }
