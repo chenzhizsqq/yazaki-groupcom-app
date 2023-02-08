98f85e6d9692632c094cff3cc77211d93e008cf7
chenzhizsqq <chenzhi@qq.com>
2023-02-08 12:08:39 +0900

    TestScannerSettingsActivity - EA520的红外线扫描Scanner 成功弄好了！！！ 直接按手机左右两边的蓝色按钮，就能够获得条形码的编码了！！！

--- app/src/main/java/com/yazaki_groupcom/app/test/TestScannerSettingsActivity.kt
+++ app/src/main/java/com/yazaki_groupcom/app/test/TestScannerSettingsActivity.kt
@@ -1,13 +1,42 @@
 package com.yazaki_groupcom.app.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import com.yazaki_groupcom.app.databinding.ActivityTestScanServiceBinding
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.yazaki_groupcom.app.databinding.ActivityTestScannerSettingsBinding
+
 
 class TestScannerSettingsActivity : AppCompatActivity() {
     companion object {
         const val TAG: String = "TestScannerSettingsActivity"
+    }
+
+    private val filter = IntentFilter("unitech.scanservice.data")
+
+    private val receiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            if (intent.action == "unitech.scanservice.data") {
+                val barcodeData = intent.getStringExtra("text")
+                Log.e(TAG, "onReceive: barcodeData:$barcodeData", )
+                Toast.makeText(context, "onReceive: barcodeData:$barcodeData", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private val runnable = Runnable {
+        try {
+            Log.e(TAG, "runnable start"  )
+            startUSS()
+            Thread.sleep(500) // USS の開始を 500 ミリ秒以上待機します
+            closeScanToKey()
+            Log.e(TAG, "runnable close"  )
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 
     private lateinit var binding: ActivityTestScannerSettingsBinding
@@ -15,5 +44,32 @@ class TestScannerSettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTestScannerSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        runnable.run()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(receiver, filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
+    }
+
+    private fun startUSS() {
+        val intent = Intent()
+        intent.action = "unitech.scanservice.start"
+        sendBroadcast(intent)
+    }
+
+    private fun closeScanToKey() {
+        val bundle = Bundle()
+        bundle.putBoolean("scan2key", false)
+        val intent = Intent()
+        intent.action = "unitech.scanservice.scan2key_setting"
+        intent.putExtras(bundle)
+        sendBroadcast(intent)
     }
 }
