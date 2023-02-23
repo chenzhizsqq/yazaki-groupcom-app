94fca46ae2f36831bb6e686b98b96581f5e5473e
chenzhizsqq <chenzhi@qq.com>
2023-02-23 16:11:59 +0900

    MainMenuActivity.kt ： 添加返回键，返回MainActivity

--- app/src/main/java/com/yazaki_groupcom/app/ui/mainMenu/MainMenuActivity.kt
+++ app/src/main/java/com/yazaki_groupcom/app/ui/mainMenu/MainMenuActivity.kt
@@ -1,8 +1,10 @@
 package com.yazaki_groupcom.app.ui.mainMenu
 
+import android.content.Intent
 import android.os.Bundle
 import com.yazaki_groupcom.app.base.BaseActivity
 import com.yazaki_groupcom.app.databinding.ActivityMainMenuBinding
+import com.yazaki_groupcom.app.ui.main.MainActivity
 
 class MainMenuActivity : BaseActivity() {
     companion object {
@@ -14,5 +16,24 @@ class MainMenuActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.returnHome.setOnClickListener {
+            val intent =
+                Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    /**
+     * 携帯電話のリターンボタンをクリックすると特定のActivityにジャンプします。
+     */
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        val intent =
+            Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
