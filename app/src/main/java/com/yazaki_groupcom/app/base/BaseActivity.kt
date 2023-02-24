88debc7bca37c341a65db7e91fa49772bbee03ed
chenzhizsqq <chenzhi@qq.com>
2023-02-24 10:55:57 +0900

    BaseActivity.kt ： 倾听是否有点击，如果有点击就把休眠时间重新计算。并把休眠时间设定暂时去掉，因为暂时对方还没到流程去做。

--- app/src/main/java/com/yazaki_groupcom/app/base/BaseActivity.kt
+++ app/src/main/java/com/yazaki_groupcom/app/base/BaseActivity.kt
@@ -5,6 +5,8 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_DOWN
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.yazaki_groupcom.app.Config
@@ -28,6 +30,9 @@ open class BaseActivity : AppCompatActivity() {
     //カウントダウン ログアウトに移行- 倒数器logout
     private var countDownTimerLogout: CountDownTimer? = null
 
+    // TODO: 是否设定倒数时间
+    private val isSettingCountTimeTime = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,12 +42,14 @@ open class BaseActivity : AppCompatActivity() {
         // タイトルバー非表示
         supportActionBar?.hide()
 
-        //設定画面を開いたままにする  スリープ状態はなし sleep off
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        //スリープ時間を設定　30分間
-        resetSleepTime()
-        //ログアウト time リセット 120分間
-        resetLogoutTime()
+        if (isSettingCountTimeTime){
+            //設定画面を開いたままにする  スリープ状態はなし sleep off
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            //スリープ時間を設定　30分間
+            resetSleepTime()
+            //ログアウト time リセット 120分間
+            resetLogoutTime()
+        }
     }
 
     //按左下角的 "三角"按钮后，触发的返回函数
@@ -54,6 +61,20 @@ open class BaseActivity : AppCompatActivity() {
     override fun finish() {
         intent.putExtra(Config.LastActivityName, this.javaClass.name)
         super.finish()
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (isSettingCountTimeTime) {
+            if (ev != null) {
+                if (ev.action == ACTION_DOWN) {
+                    //在这里处理触屏事件: ACTION_DOWN。重新设定时间
+                    resetSleepTime()
+                    resetLogoutTime()
+                }
+            }
+        }
+        // TODO: 在这里处理触屏事件
+        return super.dispatchTouchEvent(ev)
     }
 
     //スリープ time リセット
@@ -88,11 +109,11 @@ open class BaseActivity : AppCompatActivity() {
 
     //カウントダウン スリープ setting - 倒数时间的设定
     private fun countDownTimerSleepSetting(ss: Long) {
-        countDownTimerSleep = object : CountDownTimer(ss * 1000, 300) {
+        countDownTimerSleep = object : CountDownTimer(ss * 1000, 30000) {
 
             //倒数完的处理
             override fun onFinish() {
-                Log.e(TAG, "countDownTimerSleepSetting onFinish(): ")
+                Log.i(TAG, "countDownTimerSleepSetting onFinish(): ")
                 //スリープ状態に移行
                 window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             }
@@ -102,7 +123,7 @@ open class BaseActivity : AppCompatActivity() {
                 val hour = millisUntilFinished / 1000 / 60 / 60
                 val minute = millisUntilFinished / 1000 / 60 % 60
                 val second = millisUntilFinished / 1000 % 60
-                //Log.e(TAG, "countDownTimerSleepSetting onTick: 倒计时"+hour+"小时"+minute+"分"+second+"秒", )
+                Log.i(TAG, "countDownTimerSleepSetting onTick: 倒计时"+hour+"小时"+minute+"分"+second+"秒", )
 
             }
         }.start()
@@ -110,11 +131,11 @@ open class BaseActivity : AppCompatActivity() {
 
     //カウントダウン logout setting - 倒数时间的设定
     private fun countDownTimerLogoutSetting(ss: Long) {
-        countDownTimerLogout = object : CountDownTimer(ss * 1000, 300) {
+        countDownTimerLogout = object : CountDownTimer(ss * 1000, 30000) {
 
             //倒数完的处理
             override fun onFinish() {
-                Log.e(TAG, "countDownTimerLogoutSetting onFinish(): ")
+                Log.i(TAG, "countDownTimerLogoutSetting onFinish(): ")
                 //スリープ状態に移行
                 window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
@@ -130,7 +151,7 @@ open class BaseActivity : AppCompatActivity() {
                 val hour = millisUntilFinished / 1000 / 60 / 60
                 val minute = millisUntilFinished / 1000 / 60 % 60
                 val second = millisUntilFinished / 1000 % 60
-                //Log.e(TAG, "countDownTimerLogoutSetting onTick: 倒计时"+hour+"小时"+minute+"分"+second+"秒", )
+                Log.i(TAG, "countDownTimerLogoutSetting onTick: 倒计时"+hour+"小时"+minute+"分"+second+"秒", )
 
             }
         }.start()
