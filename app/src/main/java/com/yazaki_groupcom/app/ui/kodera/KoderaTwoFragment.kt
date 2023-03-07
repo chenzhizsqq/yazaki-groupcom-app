ed69a5c75206075cd40c30dad2022e98d663ee02
chenzhizsqq <chenzhi@qq.com>
2023-03-07 15:46:49 +0900

    KoderaTwoFragment - 添加QR码的识别，识别成功后，转去另外一个页面 部材変更時は部材タグの読取りを行い、切断指示と照合する。

--- app/src/main/java/com/yazaki_groupcom/app/ui/kodera/KoderaTwoFragment.kt
+++ app/src/main/java/com/yazaki_groupcom/app/ui/kodera/KoderaTwoFragment.kt
@@ -1,6 +1,8 @@
 package com.yazaki_groupcom.app.ui.kodera
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -32,11 +34,44 @@ class KoderaTwoFragment : Fragment() {
             sharedVM.idFragment.value = 3
         }
 
-        binding.etInfo.setText("1801R704140")
-        binding.etNumber.setText("004232060059")
+        sharedVM.scanDataText.value = ""
 
+        sharedVM.scanDataText.observe(viewLifecycleOwner) {
+            Log.e(TAG, " !!! QR:$it ")
+            val str = it
+            if (str.isNotEmpty()){
+                if (checkQR(str)){
+                    binding.tvResult.text = "OK"
+                    binding.etInfo.setText(str.substring(1, 12))
+                    binding.etNumber.setText(str.substring(24, 36))
+
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        sharedVM.idFragment.value = 3
+                    }, 1000) // 1000表示延时1秒钟
+                }else{
+                    binding.tvResult.text = "NG"
+                    if (str.length > 37) {
+                        binding.etInfo.setText(str.substring(1, 12))
+                        binding.etNumber.setText(str.substring(24, 36))
+                    }
+                }
+            }
+        }
 
         return binding.root
+    }
+
+    private fun checkQR(str: String):Boolean {
+        //QR:P1801R706930Z;17Q300030TO04232060059Z;23V88516D20230206Z;6Y Z;3Y001Z;52P1R7Z;53P069Z;54P30Z;11BR
+        if (str.length > 37) {
+            if (str.substring(1, 4) != "180"){
+                return false
+            }
+            return str.substring(17, 21) == "3000"
+        } else {
+            return false
+        }
+        return false
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
