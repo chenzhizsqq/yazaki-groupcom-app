5ddf3527144e168900c73ba14dae2509b07221f8
chenzhizsqq <chenzhi@qq.com>
2023-03-14 19:28:25 +0900

    发送给APK

--- app/src/main/java/com/yazaki_groupcom/app/ui/komax/KomaxOneAdapter.kt
+++ app/src/main/java/com/yazaki_groupcom/app/ui/komax/KomaxOneAdapter.kt
@@ -53,49 +53,49 @@ class KomaxOneAdapter(val context: Context) : RecyclerView.Adapter<KomaxOneAdapt
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.longSize.text = list[position].title
         holder.btCheck.setOnClickListener {
-            listener.onClick(position)
-
-            Tools.sharedPrePut("KomaxOneAdapter_type", holder.type.text.toString())
-            Tools.sharedPrePut("KomaxOneAdapter_size", holder.size.text.toString())
-            Tools.sharedPrePut("KomaxOneAdapter_color", holder.color.text.toString())
-            Tools.sharedPrePut("KomaxOneAdapter_longSize", holder.longSize.text.toString())
+//            listener.onClick(position)
+//
+//            Tools.sharedPrePut("KomaxOneAdapter_type", holder.type.text.toString())
+//            Tools.sharedPrePut("KomaxOneAdapter_size", holder.size.text.toString())
+//            Tools.sharedPrePut("KomaxOneAdapter_color", holder.color.text.toString())
+//            Tools.sharedPrePut("KomaxOneAdapter_longSize", holder.longSize.text.toString())
         }
         holder.btCheckRet.setOnClickListener {
-            val builder = AlertDialog.Builder(context)
-            builder.setTitle(context.resources.getString(R.string.cut_off_title))
-            builder.setMessage(context.resources.getString(R.string.cut_off_message))
-            builder.setPositiveButton(context.resources.getString(R.string.cut_off_ok)) { dialog, which ->
-
-                //写入文件内容
-                var fileContent = ""
-                val csvStringBuilder = StringBuilder()
-                csvStringBuilder.append("Name, Age, Gender\n")
-                csvStringBuilder.append("John, 30, Male\n")
-                csvStringBuilder.append("Jane, 25, Female\n")
-                csvStringBuilder.append("Bob, 40, Male\n")
-                fileContent = csvStringBuilder.toString()
-
-                //用文字内容写成csv
-                Tools.makeCsv(fileContent)
-
-                //change state
-                holder.btCheck.changeColorByState(3)
-                holder.btCheckRet.changeColorByState(3)
-
-                //change color
-                holder.type.setBackgroundResource(R.drawable.bg_layout_black)
-                holder.size.setBackgroundResource(R.drawable.bg_layout_black)
-                holder.color.setBackgroundResource(R.drawable.bg_layout_black)
-                holder.longSize.setBackgroundResource(R.drawable.bg_layout_black)
-
-
-            }
-            builder.setNegativeButton(context.resources.getString(R.string.cut_off_cancel)) { dialog, which ->
-                // 点击 Cancel 按钮的回调
-            }
-            val dialog = builder.create()
-            dialog.show()
-            listener.onCheck(position)
+//            val builder = AlertDialog.Builder(context)
+//            builder.setTitle(context.resources.getString(R.string.cut_off_title))
+//            builder.setMessage(context.resources.getString(R.string.cut_off_message))
+//            builder.setPositiveButton(context.resources.getString(R.string.cut_off_ok)) { dialog, which ->
+//
+//                //写入文件内容
+//                var fileContent = ""
+//                val csvStringBuilder = StringBuilder()
+//                csvStringBuilder.append("Name, Age, Gender\n")
+//                csvStringBuilder.append("John, 30, Male\n")
+//                csvStringBuilder.append("Jane, 25, Female\n")
+//                csvStringBuilder.append("Bob, 40, Male\n")
+//                fileContent = csvStringBuilder.toString()
+//
+//                //用文字内容写成csv
+//                Tools.makeCsv(fileContent)
+//
+//                //change state
+//                holder.btCheck.changeColorByState(3)
+//                holder.btCheckRet.changeColorByState(3)
+//
+//                //change color
+//                holder.type.setBackgroundResource(R.drawable.bg_layout_black)
+//                holder.size.setBackgroundResource(R.drawable.bg_layout_black)
+//                holder.color.setBackgroundResource(R.drawable.bg_layout_black)
+//                holder.longSize.setBackgroundResource(R.drawable.bg_layout_black)
+//
+//
+//            }
+//            builder.setNegativeButton(context.resources.getString(R.string.cut_off_cancel)) { dialog, which ->
+//                // 点击 Cancel 按钮的回调
+//            }
+//            val dialog = builder.create()
+//            dialog.show()
+//            listener.onCheck(position)
         }
     }
 
